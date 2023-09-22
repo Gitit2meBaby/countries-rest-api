72e341aa804df654fcf37bb1d82186e6813b2aa0
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-23 00:26:55 +0700

    becoming a bit code soupesque.. issues include click on card not working after filtering by region, search box selector now broken, search works but selecting an item yields a blank page. Still need to do dark mode, create links on the border buttons, clean up css and try to refactor some javascript.

--- script.js
+++ script.js
@@ -1,117 +1,143 @@
 document.addEventListener('DOMContentLoaded', () => {
     let jsonData;
     let countryNames = [];
-
+    let cards;
 
     async function fetchAPI() {
         try {
             const response = await fetch('https://restcountries.com/v3.1/all');
             jsonData = await response.json();
             console.log('API loaded successfully:');
+            console.log('jsonData:', jsonData); // Log the entire jsonData array for inspection
 
+            countryNames = jsonData.map(item => item.name.common);
 
             // CREATE CARDS ON PAGE LOAD
-            jsonData.forEach(item => {
-                const cardContainer = document.getElementById('cardContainer');
+            function createCards() {
+                jsonData.forEach(item => {
 
-                const card = document.createElement('div');
-                card.innerHTML = cardTemplate(
-                    item.name.common,
-                    item.population,
-                    item.region,
-                    item.capital,
+                    const card = document.createElement('div');
+                    card.innerHTML = cardTemplate(
+                        item.name.common,
+                        item.population,
+                        item.region,
+                        item.capital,
+                        item.flags.png,
+                        `${item.name.common} Flag`
+                    );
+
+                    cardContainer.appendChild(card);
+                });
+            }
+
+            createCards()
+            cards = document.querySelectorAll('.card'); // Assign cards here
+
+
+            // function to create the selected country page
+            function createSelectedCountryElement(item) {
+                const selectedCountryElement = document.createElement('div');
+
+                // Parse JSON data into a usable format (and deal with outliers)
+                const languages = Object.keys(item.languages).map(languageCode => {
+                    return item.languages[languageCode];
+                }).join(', ');
+
+                const currencies = item.currencies ? Object.keys(item.currencies).map(currencyCode => {
+                    const currency = item.currencies[currencyCode];
+                    return `${currency.name} (${currency.symbol})`;
+                }).join(', ') : 'N/A';
+
+                const nativeName = getOfficialNativeName(item.name.nativeName);
+                function getOfficialNativeName(nativeName) {
+                    for (const langCode in nativeName) {
+                        if (nativeName[langCode].official) {
+                            return nativeName[langCode].official;
+                        }
+                    }
+                    return '';
+                }
+
+                selectedCountryElement.innerHTML = selectedCountryTemplate(
                     item.flags.png,
-                    `${item.name.common} Flag`
+                    `${item.name.common} Flag`,
+                    item.name.common,
+                    nativeName || 'N/A',
+                    item.population || 'N/A',
+                    item.region || 'N/A',
+                    item.subregion || 'N/A',
+                    item.capital || 'N/A',
+                    item.tld.join(', ') || 'N/A',
+                    currencies || 'N/A',
+                    languages || 'N/A',
                 );
 
-                cardContainer.appendChild(card);
-            });
+                return selectedCountryElement;
+            }
 
-            // ADD AN EVENTLISTENER TO EACH CARD
-            const cards = document.querySelectorAll('.card')
+            //event listener when clicking on a card
             cards.forEach((card, index) => {
                 card.addEventListener('click', () => {
                     const item = jsonData[index];
-                    console.log(jsonData[index])
-                    const chosenCountryGrid = document.querySelector('.chosen-country-grid');
-                    const chosenCountry = document.querySelector('#chosenCountry');
+                    chosenCountryGrid = document.querySelector('.chosen-country-grid')
+                    const selectedCountryElement = createSelectedCountryElement(item);
 
-                    // Create a new DOM element for selectedCountryHTML
-                    const selectedCountryElement = document.createElement('div');
-
-                    //Parse JSON data into a useable format
-                    const languages = Object.keys(item.languages).map(languageCode => {
-                        return item.languages[languageCode];
-                    }).join(', ');
-
-                    const currencies = item.currencies ? Object.keys(item.currencies).map(currencyCode => {
-                        const currency = item.currencies[currencyCode];
-                        return `${currency.name} (${currency.symbol})`;
-                    }).join(', ') : 'N/A';
-
-                    const nativeName = getOfficialNativeName(item.name.nativeName);
-                    function getOfficialNativeName(nativeName) {
-                        for (const langCode in nativeName) {
-                            if (nativeName[langCode].official) {
-                                return nativeName[langCode].official;
-                            }
-                        }
-                        return ''; // Return an empty string if not found
-                    }
-
-
-                    selectedCountryElement.innerHTML = selectedCountryTemplate(
-                        item.flags.png,
-                        `${item.name.common} Flag`,
-                        item.name.common,
-                        nativeName || 'N/A',
-                        item.population || 'N/A',
-                        item.region || 'N/A',
-                        item.subregion || 'N/A',
-                        item.capital || 'N/A',
-                        item.tld.join(', ') || 'N/A',
-                        currencies || 'N/A',
-                        languages || 'N/A',
-                        item.borderCountries
-                    );
 
                     displaySelectedCountry(selectedCountryElement);
                     chosenCountry.classList.remove('hidden');
-
-                    // Clear previous content in chosenCountryGrid
                     chosenCountryGrid.innerHTML = '';
-
-                    // Append the new DOM element to chosenCountryGrid
                     chosenCountryGrid.appendChild(selectedCountryElement);
+                    if (item.borders) {
+                        createBorderButtons(jsonData, index, item.borders);
+                    }
                 });
             });
 
 
             // Function to display the selected country HTML 
+            const mainPage = document.querySelector('.home');
             function displaySelectedCountry(selectedCountryElement) {
-                const mainPage = document.querySelector('.home');
                 mainPage.classList.add('hidden');
             }
 
+            function createSuggestions() {
+                const search = document.querySelector('#search');
+                const suggestionContainer = document.querySelector('.suggestion-container');
+                suggestionContainer.classList.add('suggestion')
+                const inputValue = search.value.toLowerCase();
+                const matchingCountries = countryNames.filter(country =>
+                    country.toLowerCase().startsWith(inputValue)
+                );
+
+                clearSuggestions();
+
+                matchingCountries.forEach((countryName, index) => {
+                    const suggestionText = document.createElement('p');
+                    const chosenCountryGrid = document.querySelector('.chosen-country-grid')
+                    suggestionText.textContent = countryName;
+                    suggestionText.classList.add('suggestion-text');
+                    suggestionContainer.appendChild(suggestionText);
 
 
+                    // Add a click event listener to each suggestion
+                    suggestionText.addEventListener('click', () => {
+                        const item = jsonData[index];
+                        const selectedCountryElement = createSelectedCountryElement(item);
+
+                        displaySelectedCountry(selectedCountryElement);
+                        chosenCountryGrid.classList.remove('hidden');
+                        chosenCountry.classList.remove('hidden');
+                        chosenCountryGrid.innerHTML = '';
+                        chosenCountryGrid.appendChild(selectedCountryElement);
+                    });
+                });
+            }
+
+            // Attach a keyup event listener to the search input
+            search.addEventListener('keyup', createSuggestions);
 
 
-
-
-            // SEARCH BAR EVENT LISTENER
-            const search = document.querySelector('#search');
-            const suggestionContainer = document.querySelector('.suggestion-container')
-            search.addEventListener('keyup', handleSearch)
-            countryNames = jsonData.map(item => item.name.common);
-
-            // Handle arrow key events on the input field
-            search.addEventListener('keydown', (event) => {
-                if (event.key === 'ArrowDown') {
-                    suggestionContainer.focus();
-                }
-            });
-
+            // function to look through JSON and match names to input text
             function handleSearch() {
                 const inputValue = search.value.toLowerCase();
                 const matchingCountries = countryNames.filter(country =>
@@ -119,6 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 );
 
                 clearSuggestions();
+
                 // creating the searchbar dropdown
                 const suggestion = document.createElement('div');
                 matchingCountries.forEach(countryName => {
@@ -141,40 +168,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
 
+            //event listener on suggestions
+            const suggestionText = document.querySelectorAll('.suggestion-text');
+
+            suggestionText.forEach((suggestion, index) => {
+                console.log('Adding click listener to suggestion', index);
+                suggestion.addEventListener('click', () => {
+                    const item = jsonData[index];
+                    const selectedCountryElement = createSelectedCountryElement(item);
+
+                    console.log('Suggestion clicked');
+                    displaySelectedCountry(selectedCountryElement);
+                    chosenCountry.classList.remove('hidden');
+                    chosenCountryGrid.innerHTML = '';
+                    chosenCountryGrid.appendChild(selectedCountryElement);
+                    createBorderButtons(jsonData, index)
+                });
+            });
+
             function clearSuggestions() {
+                const suggestionContainer = document.querySelector('.suggestion-container');
                 while (suggestionContainer.firstChild) {
                     suggestionContainer.removeChild(suggestionContainer.firstChild);
                 }
             }
 
-            // Add an event listener to the new div
-            suggestionContainer.addEventListener('keydown', handleKeyDown);
-
-            // Initialize a variable to keep track of the currently selected suggestion
-            let selectedSuggestionIndex = -1;
-            let suggestions = document.querySelectorAll('suggestion-text')
-
-            function handleKeyDown(event) {
-                const suggestions = document.querySelectorAll('.suggestion');
-
-                if (event.key === 'ArrowDown') {
-                    selectedSuggestionIndex = (selectedSuggestionIndex + 1) % suggestions.length;
-                } else if (event.key === 'ArrowUp') {
-                    selectedSuggestionIndex = (selectedSuggestionIndex - 1 + suggestions.length) % suggestions.length;
-                }
-            }
-
-            // Add a click event listener to handle suggestion selection
-            suggestions.forEach((suggestion, index) => {
-                suggestion.addEventListener('click', () => {
-                    search.value = suggestion.textContent;
-                    clearSuggestions();
-                });
-            });
-
             // HANDLE REGION FILTER WITH DROPDOWN
             const regionDropdown = document.querySelector('#dropdown');
-            const cardContainer = document.getElementById('cardContainer');
 
             regionDropdown.addEventListener('change', () => {
                 const selectedRegion = regionDropdown.value;
@@ -200,6 +220,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             });
 
+            function createBorderButtons(jsonData, index, borderAlpha3Codes) {
+                const currentCountry = jsonData[index];
+                const buttonContainer = document.querySelector('.border-countries-btn-wrapper');
+
+                // Create buttons for each bordering country
+                borderAlpha3Codes.forEach((alpha3Code) => {
+                    // Find the country object with the matching alpha3Code
+                    const borderCountry = jsonData.find((country) => country.cca3 === alpha3Code); // Use 'cca3' property for matching
+
+                    if (borderCountry) {
+                        const button = document.createElement('button');
+                        button.classList.add('primary-btn', 'border-btn')
+                        button.textContent = borderCountry.name.common;
+                        buttonContainer.appendChild(button);
+                    } else {
+                        console.log(`No country found for alpha3Code: ${alpha3Code}`);
+                    }
+                });
+            }
+
+
+
         } catch (error) {
             console.error('Error fetching API:', error);
         }
@@ -222,8 +264,8 @@ const cardTemplate = (name, population, region, capital, flagSrc, flagAlt) => `
 `;
 
 // SELECTED COUNTRY HTML TEMPLATE
-const selectedCountryTemplate = (flagSrc, flagAlt, name, nativeName, population, region, subRegion, capital, topLevelDomain, currencies, languages, borderCountries) => `
-<div class="flag-image">
+const selectedCountryTemplate = (flagSrc, flagAlt, name, nativeName, population, region, subRegion, capital, topLevelDomain, currencies, languages, createBorderButtons) => `
+            <div class="flag-image">
                 <img src="${flagSrc}" alt="${flagAlt}">
             </div>
 
@@ -254,25 +296,19 @@ const selectedCountryTemplate = (flagSrc, flagAlt, name, nativeName, population,
             </div>
 `;
 
+// BACK BUTTON TO RETURN TO HOME SCREEN
+const back = document.querySelector('#back');
+const mainPage = document.querySelector('.home');
+
+back.addEventListener('click', () => {
+    chosenCountry.classList.add('hidden');
+    mainPage.classList.remove('hidden');
 
 
-
-
-
-
-
-
-
-
-
+})
 
 const lightModeMoon = document.querySelector('#lightModeMoon');
 const darkModeMoon = document.querySelector('#darkModeMoon');
 const modeText = document.querySelector('#modeText');
 
 
-const dropdown = document.querySelector('#dropdown');
-
-const back = document.querySelector('#back');
-
-const borderCountriesBtnWrapper = document.querySelector('.border-countries-btn-wrapper');
